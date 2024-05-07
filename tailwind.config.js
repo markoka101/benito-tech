e2050978f94a3788e7ef75cf463dcdf25a73276e
markoka101 <markoka101@gmail.com>
2024-05-07 17:14:24 -0400

    set tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'custom-bg' : '#0e253b'
+            }
+        }
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: [],
+}
